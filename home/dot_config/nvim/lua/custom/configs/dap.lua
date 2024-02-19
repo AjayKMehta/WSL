000598a5707cf889d35ed46b259986c678ede38e
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-02-18 17:19:03 -0800

    Add debugging for Lua.

--- home/dot_config/nvim/lua/custom/configs/dap.lua
+++ home/dot_config/nvim/lua/custom/configs/dap.lua
@@ -181,3 +181,15 @@ dap.configurations.haskell = {
 		ghciCmd = "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
 	},
 }
+
+dap.configurations.lua = {
+	{
+		type = "nlua",
+		request = "attach",
+		name = "Attach to running Neovim instance",
+	},
+}
+
+dap.adapters.nlua = function(callback, config)
+	callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+end
