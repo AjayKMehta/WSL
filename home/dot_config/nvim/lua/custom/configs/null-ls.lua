0e7e6784f745f9d816fda38737007b9664143ec1
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-02-29 11:21:40 -0800

    Remove deprecated builtins.
    See https://github.com/nvimtools/none-ls.nvim/issues/58

--- home/dot_config/nvim/lua/custom/configs/null-ls.lua
+++ home/dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -11,54 +11,26 @@ local actions = b.code_actions
 local comp = b.completion
 
 local sources = {
-	-- webdev stuff
-	formatting.deno_fmt, -- chose deno for ts/js files cuz its very fast!
 	formatting.prettier.with({ filetypes = { "html", "css" } }), -- so prettier works only on these filetypes
 
 	-- Lua
 	formatting.stylua,
 	comp.luasnip,
-	lint.luacheck.with({ extra_args = { "--globals", "vim" } }),
 
 	-- cpp
 	formatting.clang_format,
 	-- https://github.com/jose-elias-alvarez/null-ls.nvim
 	actions.gitsigns,
 
-	-- cspell
-	lint.cspell.with({ filetypes = { "markdown", "text" } }),
-	actions.cspell,
-	formatting.trim_whitespace,
-
-	-- Shell
-	actions.shellcheck,
-	lint.shellcheck,
-
-	-- LaTeX
-	lint.chktex,
-	formatting.latexindent,
-
 	-- Markdown
 	lint.markdownlint,
 	formatting.markdownlint,
-	formatting.markdown_toc,
-
-	-- Python
-	lint.mypy,
-	lint.ruff,
-	formatting.ruff,
 
 	lint.todo_comments,
 	actions.refactoring,
 
 	-- JSON, YAML, XML
 	lint.yamllint,
-	formatting.jq,
-	formatting.xq,
-	formatting.yq,
-
-	-- Haskell
-	formatting.brittany,
 
 	formatting.format_r,
 
