75f850455451e91a4171483aaffb4c42cd683d10
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-07 20:14:12 -0800

    Update conform config.

--- home/dot_config/nvim/lua/custom/configs/conform.lua
+++ home/dot_config/nvim/lua/custom/configs/conform.lua
@@ -1,6 +1,29 @@
 local conform = require("conform")
 
 conform.setup({
+	formatters = {
+		-- stack install cabal-fmt
+		-- Not listed in Mason registry.
+		cabal_fmt = {
+			meta = {
+				url = "https://hackage.haskell.org/package/cabal-fmt",
+				description = "Format cabal files with cabal-fmt",
+			},
+			command = "cabal-fmt",
+		},
+		cbfmt = {
+			prepend_args = {
+				"--config",
+				vim.env.HOME .. "/.cbfmt.toml",
+			},
+		},
+		["markdownlint-cli"] = {
+			prepend_args = {
+				"--config",
+				vim.env.HOME .. "/.markdownlint.json",
+			},
+		},
+	},
 	format_on_save = function(bufnr)
 		-- Disable with a global or buffer-local variable
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
@@ -11,19 +34,31 @@ conform.setup({
 	-- If you specify more than one formatter, they will be executed in the order you list them.
 	formatters_by_ft = {
 		bash = { "shellcheck" },
-		json = { "jq", "fixjson" },
+		-- TODO: Figure out how to use dotnet format instead
+		-- cs = { "csharpier" },
+		cabal = { "cabal_fmt" },
+		haskell = { "fourmolu" },
+		json = { "fixjson" },
+		json5 = { "fixjson" },
+		jsonc = { "fixjson" },
 		lua = { "stylua" },
-		markdown = { "markdownlint" },
+		markdown = { "markdownlint", "cbfmt" },
 		python = { "ruff" },
 		sql = { "sql-formatter" },
 		tex = { "latexindent" },
 		toml = { "taplo" },
 		yaml = { "yamlfix" },
+		-- Use the "*" filetype to run formatters on all filetypes.
+		-- ["*"] = { },
+		-- Use the "_" filetype to run formatters on filetypes that don't
+		-- have other formatters configured.
+		["_"] = { "trim_whitespace" },
 	},
 })
 
 -- Keymap
 
+-- In normal mode it will apply to the whole file, in visual mode it will apply to the current selection.
 vim.keymap.set({ "n", "v" }, "<leader>lf", function()
 	conform.format({
 		lsp_fallback = true,
