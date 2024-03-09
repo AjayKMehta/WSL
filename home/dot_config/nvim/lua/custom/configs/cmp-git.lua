af30882bd54ea1900bf6edf71e7ba90f20e79926
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-09 14:36:08 -0800

    Add gitrebase.

--- home/dot_config/nvim/lua/custom/configs/cmp-git.lua
+++ home/dot_config/nvim/lua/custom/configs/cmp-git.lua
@@ -1,6 +1,6 @@
 local cmp = require("cmp")
 ---@diagnostic disable-next-line: missing-fields
-cmp.setup.filetype("gitcommit", {
+cmp.setup.filetype({ "gitcommit", "gitrebase" }, {
 	sources = cmp.config.sources({
 		{ name = "cmp_git", priority = 100 },
 		{ name = "async_path", priority = 50 },
