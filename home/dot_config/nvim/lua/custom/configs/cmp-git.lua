3f4b8cd24783aeb08bf013eb36674786ef7c01ca
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-09 14:30:21 -0800

    Fix git completions.

--- home/dot_config/nvim/lua/custom/configs/cmp-git.lua
+++ home/dot_config/nvim/lua/custom/configs/cmp-git.lua
@@ -2,9 +2,8 @@ local cmp = require("cmp")
 ---@diagnostic disable-next-line: missing-fields
 cmp.setup.filetype("gitcommit", {
 	sources = cmp.config.sources({
-		{ name = "git", priority = 50 },
-		{ name = "async_path", priority = 40 },
-	}, {
+		{ name = "cmp_git", priority = 100 },
+		{ name = "async_path", priority = 50 },
 		{ name = "buffer", priority = 50 },
 		{ name = "emoji", insert = true, priority = 20 },
 	}),
