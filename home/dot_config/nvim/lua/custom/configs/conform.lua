5da5c43fe7c3937550fcc30e70d3a0b7c32e3ea1
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-09 10:12:34 -0800

    Update conform settings.

--- home/dot_config/nvim/lua/custom/configs/conform.lua
+++ home/dot_config/nvim/lua/custom/configs/conform.lua
@@ -23,10 +23,28 @@ conform.setup({
 				vim.env.HOME .. "/.markdownlint.json",
 			},
 		},
+		injected = {
+			-- Set the options field
+			options = {
+				lang_to_formatters = {
+					python = "ruff",
+				},
+			},
+		},
 	},
 	format_on_save = function(bufnr)
+		-- Disable autoformat on certain filetypes
+		local ignore_filetypes = { "sql", "python" }
+		if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+			return
+		end
 		-- Disable with a global or buffer-local variable
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		-- Disable autoformat for files in a certain path
+		local bufname = vim.api.nvim_buf_get_name(bufnr)
+		if bufname:match("/node_modules/") then
 			return
 		end
 		return { timeout_ms = 500, lsp_fallback = true }
