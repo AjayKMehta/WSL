c101ca3b87ab556251f739028b8c38375d62cc16
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-15 17:25:36 -0700

    Add keymap to close hover window.

--- home/dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ home/dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -231,3 +231,26 @@ lspconfig.texlab.setup({
 		},
 	},
 })
+
+-- https://vi.stackexchange.com/a/43436
+local hover_close = function(base_win_id)
+	local windows = vim.api.nvim_tabpage_list_wins(0)
+	for _, win_id in ipairs(windows) do
+		if win_id ~= base_win_id then
+			local win_cfg = vim.api.nvim_win_get_config(win_id)
+			if win_cfg.relative == "win" and win_cfg.win == base_win_id then
+				vim.api.nvim_win_close(win_id, {})
+				break
+			end
+		end
+	end
+end
+
+-- Later, or in another file, when you create keymaps for LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		vim.keymap.set("n", "<Leader>;", function()
+			hover_close(vim.api.nvim_get_current_win())
+		end, { remap = false, silent = true, buffer = ev.buf, desc = "Close hover window" })
+	end,
+})
