6d0dd52f96085c7dcc0a605363cc03f7e2d29ebd
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-16 07:53:46 -0700

    :recycle: Cleanup

--- home/dot_config/nvim/lua/custom/sniputils.lua
+++ home/dot_config/nvim/lua/custom/sniputils.lua
@@ -45,11 +45,14 @@ end
 utils.bash = function(_, _, command)
 	local file = io.popen(command, "r")
 	local res = {}
-	for line in file:lines() do
-		table.insert(res, line)
+	if file ~= nil then
+		for line in file:lines() do
+			table.insert(res, line)
+		end
 	end
 	return res
 end
+
 utils.part = function(func, ...)
 	local args = { ... }
 	return function()
@@ -216,85 +219,6 @@ utils.luadocsnip = function(args, _, old_state)
 	return snip
 end
 
--- complicated function for dynamicNode.
-utils.cppdocsnip = function(args, _, old_state)
-	dump(args)
-	-- !!! old_state is used to preserve user-input here. DON'T DO IT THAT WAY!
-	-- Using a restoreNode instead is much easier.
-	-- View this only as an example on how old_state functions.
-	local nodes = {
-		t({ "/**", " * " }),
-		i(1, "A short Description"),
-		t({ "", "" }),
-	}
-
-	-- These will be merged with the snippet; that way, should the snippet be updated,
-	-- some user input eg. text can be referred to in the new snippet.
-	local param_nodes = {}
-
-	if old_state then
-		nodes[2] = i(1, old_state.descr:get_text())
-	end
-	param_nodes.descr = nodes[2]
-
-	-- At least one param.
-	if string.find(args[2][1], ", ") then
-		vim.list_extend(nodes, { t({ " * ", "" }) })
-	end
-
-	local insert = 2
-	for indx, arg in ipairs(vim.split(args[2][1], ", ", true)) do
-		-- Get actual name parameter.
-		arg = vim.split(arg, " ", true)[2]
-		if arg then
-			local inode
-			-- if there was some text in this parameter, use it as static_text for this new snippet.
-			if old_state and old_state[arg] then
-				inode = i(insert, old_state["arg" .. arg]:get_text())
-			else
-				inode = i(insert)
-			end
-			vim.list_extend(nodes, { t({ " * @param " .. arg .. " " }), inode, t({ "", "" }) })
-			param_nodes["arg" .. arg] = inode
-
-			insert = insert + 1
-		end
-	end
-
-	if args[1][1] ~= "void" then
-		local inode
-		if old_state and old_state.ret then
-			inode = i(insert, old_state.ret:get_text())
-		else
-			inode = i(insert)
-		end
-
-		vim.list_extend(nodes, { t({ " * ", " * @return " }), inode, t({ "", "" }) })
-		param_nodes.ret = inode
-		insert = insert + 1
-	end
-
-	if vim.tbl_count(args[3]) ~= 1 then
-		local exc = string.gsub(args[3][2], " throws ", "")
-		local ins
-		if old_state and old_state.ex then
-			ins = i(insert, old_state.ex:get_text())
-		else
-			ins = i(insert)
-		end
-		vim.list_extend(nodes, { t({ " * ", " * @throws " .. exc .. " " }), ins, t({ "", "" }) })
-		param_nodes.ex = ins
-		insert = insert + 1
-	end
-
-	vim.list_extend(nodes, { t({ " */" }) })
-
-	local snip = sn(nil, nodes)
-	-- Error on attempting overwrite.
-	snip.old_state = param_nodes
-	return snip
-end
-
 utils.env = function(name)
 	local x, y = table.unpack(vim.fn["vimtex#env#is_inside"](name))
 	return x ~= "0" and y ~= "0"
@@ -307,6 +231,7 @@ end
 utils.not_math = function()
 	return not utils.is_math()
 end
+
 utils.is_math = function()
 	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 	-- return in_mathzone()
