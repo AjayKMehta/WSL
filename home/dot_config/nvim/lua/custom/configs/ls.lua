c1aeb91436caae2ad8fd1903b372d3aacf95e461
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-19 00:57:18 -0700

    :memo: Add comments.

--- home/dot_config/nvim/lua/custom/configs/ls.lua
+++ home/dot_config/nvim/lua/custom/configs/ls.lua
@@ -11,6 +11,8 @@ local opts = {
 	region_check_events = "CursorMoved,CursorHold,InsertEnter",
 	enable_autosnippets = true,
 	history = true,
+	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
 	store_selection_keys = "<Tab>",
 	ft_func = ft_func.from_pos_or_filetype,
 	load_ft_func = ft_func.extend_load_ft({
