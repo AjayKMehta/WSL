7a07bb1444efc37f0d2169e1a75774b689f169e1
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-19 19:47:44 -0700

    Fix snippet path.

--- home/dot_config/nvim/lua/configs/ls.lua
+++ home/dot_config/nvim/lua/configs/ls.lua
@@ -1,59 +1,59 @@
 local ls = require("luasnip")
-local ft_func = require("luasnip.extras.filetype_functions")
 local extras = require("luasnip.extras")
+local ft_func = require("luasnip.extras.filetype_functions")
 
 local fmt = require("luasnip.extras.fmt").fmt
 
 -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#config-options
 local opts = {
-	updateevents = "TextChanged,TextChangedI",
-	delete_check_events = "TextChanged,CursorMoved",
-	region_check_events = "CursorMoved,CursorHold,InsertEnter",
-	enable_autosnippets = true,
-	history = true,
-	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
-	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
-	store_selection_keys = "<Tab>",
-	ft_func = ft_func.from_pos_or_filetype,
-	load_ft_func = ft_func.extend_load_ft({
-		markdown = { "lua", "json", "html", "yaml", "css", "html", "javascript", "r", "python" },
-		html = { "javascript", "css", "graphql", "json" },
-	}),
-	ext_opts = {
-		[require("luasnip.util.types").choiceNode] = {
-			active = {
-				virt_text = { { "●", "St_SelectMode" } },
-			},
-		},
-		[require("luasnip.util.types").insertNode] = {
-			active = {
-				virt_text = { { "⎀", "St_InsertMode" } },
-			},
-		},
-	},
-	snip_env = {
-		fmt = fmt,
-		m = extras.match,
-		t = ls.text_node,
-		f = ls.function_node,
-		c = ls.choice_node,
-		d = ls.dynamic_node,
-		i = ls.insert_node,
-		l = extras.lambda,
-		s = ls.snippet,
-		sn = ls.snippet_node,
-	},
+    updateevents = "TextChanged,TextChangedI",
+    delete_check_events = "TextChanged,CursorMoved",
+    region_check_events = "CursorMoved,CursorHold,InsertEnter",
+    enable_autosnippets = true,
+    history = true,
+    -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+    -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+    store_selection_keys = "<Tab>",
+    ft_func = ft_func.from_pos_or_filetype,
+    load_ft_func = ft_func.extend_load_ft({
+        markdown = { "lua", "json", "html", "yaml", "css", "html", "javascript", "r", "python" },
+        html = { "javascript", "css", "graphql", "json" },
+    }),
+    ext_opts = {
+        [require("luasnip.util.types").choiceNode] = {
+            active = {
+                virt_text = { { "●", "St_SelectMode" } },
+            },
+        },
+        [require("luasnip.util.types").insertNode] = {
+            active = {
+                virt_text = { { "⎀", "St_InsertMode" } },
+            },
+        },
+    },
+    snip_env = {
+        fmt = fmt,
+        m = extras.match,
+        t = ls.text_node,
+        f = ls.function_node,
+        c = ls.choice_node,
+        d = ls.dynamic_node,
+        i = ls.insert_node,
+        l = extras.lambda,
+        s = ls.snippet,
+        sn = ls.snippet_node,
+    },
 }
 
 ls.setup(opts)
 
-local parent_path = vim.fn.stdpath("config") .. "/lua/custom/"
+local parent_path = vim.fn.stdpath("config") .. "/lua/"
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load({
-	paths = { parent_path .. "snippets" },
+    paths = { parent_path .. "snippets" },
 })
 require("luasnip.loaders.from_lua").lazy_load({
-	paths = parent_path .. "lua_snippets",
+    paths = parent_path .. "lua_snippets",
 })
 
 -- friendly-snippets - enable standardized comments snippets
