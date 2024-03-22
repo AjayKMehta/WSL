5b98fe4988b5ee3d5aa1036bb826ce829a5a36b0
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-21 19:49:31 -0700

    Move debugging plugins to new file.

---
+++ home/dot_config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,41 @@
+local overrides = require("configs.overrides")
+
+local function load_config(plugin)
+    return function()
+        require("configs." .. plugin)
+    end
+end
+
+return {
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = load_config("dap-ui"),
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = load_config("dap"),
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function(_, opts)
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            require("dap-python").setup(path)
+        end,
+    },
+    {
+        -- It's important that you set up the plugins in the following order:
+
+        -- 1. mason.nvim
+        -- 2. mason-nvim-dap.nvim
+
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = load_config("mason-nvim-dap"),
+    },
+}
--- home/dot_config/nvim/lua/plugins/myplugins.lua
+++ home/dot_config/nvim/lua/plugins/myplugins.lua
@@ -31,27 +31,7 @@ Plugins divided into the following categories:
 
 local plugins = {
     --#region Debugging, diagnostics
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        config = load_config("dap-ui"),
-    },
-    {
-        "mfussenegger/nvim-dap",
-        config = load_config("dap"),
-    },
-    {
-        "mfussenegger/nvim-dap-python",
-        ft = "python",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
-        },
-        config = function(_, opts)
-            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-            require("dap-python").setup(path)
-        end,
-    },
+
     -- It's important that you set up the plugins in the following order:
 
     -- mason.nvim
@@ -71,16 +51,6 @@ local plugins = {
         config = function(_, opts)
             require("mason-lspconfig").setup(opts)
         end,
-    },
-    {
-        -- It's important that you set up the plugins in the following order:
-
-        -- 1. mason.nvim
-        -- 2. mason-nvim-dap.nvim
-
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = load_config("mason-nvim-dap"),
     },
     {
         "aznhe21/actions-preview.nvim",
