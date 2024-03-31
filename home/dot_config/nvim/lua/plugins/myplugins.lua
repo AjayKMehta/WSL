8012de30d7168e6c114bba640e9857c3bc659739
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-03-31 09:41:39 -0700

    :art: Fix whitespace.

--- home/dot_config/nvim/lua/plugins/myplugins.lua
+++ home/dot_config/nvim/lua/plugins/myplugins.lua
@@ -19,7 +19,7 @@ Plugins divided into the following categories:
 ---@diagnostic disable-next-line: undefined-doc-name
 
 local plugins = {
-	--#region Treesitter + LSP
+    --#region Treesitter + LSP
 
 	{
 		-- Pretty hover messages.
@@ -28,110 +28,110 @@ local plugins = {
 	},
 	{ "poljar/typos.nvim", lazy = false },
 
-	{
-		-- A framework for running functions on Tree-sitter nodes, and updating the buffer with the result.
-		"ckolkey/ts-node-action",
-		dependencies = { "nvim-treesitter" },
-		opts = {},
-	},
-	{
-		-- Enhanced folds
-		"kevinhwang91/nvim-ufo",
-		dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
-		event = "BufRead",
-		config = load_config("ufo"),
-	},
-	{
-		-- Navigate your code with search labels, enhanced character motions, and Treesitter integration.
-		"folke/flash.nvim",
-		event = "VeryLazy",
-	},
+    {
+        -- A framework for running functions on Tree-sitter nodes, and updating the buffer with the result.
+        "ckolkey/ts-node-action",
+        dependencies = { "nvim-treesitter" },
+        opts = {},
+    },
+    {
+        -- Enhanced folds
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
+        event = "BufRead",
+        config = load_config("ufo"),
+    },
+    {
+        -- Navigate your code with search labels, enhanced character motions, and Treesitter integration.
+        "folke/flash.nvim",
+        event = "VeryLazy",
+    },
 
-	{
-		-- Advanced comment plugin with Treesitter support.
-		"numToStr/Comment.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		config = load_config("comment"),
-		dependencies = {
-			{
-				"JoosepAlviste/nvim-ts-context-commentstring",
-				lazy = false,
-				config = function()
-	-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
-	require("ts_context_commentstring").setup({
-		enable_autocmd = false,
-	})
-	vim.g.skip_ts_context_commentstring_module = true
-				end,
-			},
-		},
-	},
+    {
+        -- Advanced comment plugin with Treesitter support.
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        config = load_config("comment"),
+        dependencies = {
+            {
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                lazy = false,
+                config = function()
+                    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
+                    require("ts_context_commentstring").setup({
+                        enable_autocmd = false,
+                    })
+                    vim.g.skip_ts_context_commentstring_module = true
+                end,
+            },
+        },
+    },
 
-	{
-		-- Generate comments based on treesitter.
-		"danymat/neogen",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		opts = overrides.neogen,
-		-- Uncomment next line if you want to follow only stable versions
-		version = "*",
-		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = overrides.treesitter,
-		event = { "BufReadPre", "BufNewFile" },
-		cmd = {
-			"TSInstall",
-			"TSInstallSync",
-			"TSInstallInfo",
-			"TSUpdate",
-			"TSUpdateSync",
-			"TSUninstall",
-		},
-		build = function()
-	require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-refactor",
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function(_, opts)
-	dofile(vim.g.base46_cache .. "syntax")
-	dofile(vim.g.base46_cache .. "treesitter")
-	require("nvim-treesitter.configs").setup(opts)
-	require("nvim-treesitter.install").compilers = { "clang" }
-		end,
-	},
-	{
-		-- Show code context.,
-		"nvim-treesitter/nvim-treesitter-context",
-		lazy = false,
-		event = "BufReadPost",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = overrides.treesitter_context,
-	},
-	{
-		-- Syntax aware text-objects, select, move, swap, and peek support.
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = {
-			-- "nvim-treesitter/nvim-treesitter",
-		},
-		config = load_config("ts_textobjects"),
-	},
-	{
-		-- Helps you navigate and move nodes around based on Treesitter API.
-		"ziontee113/syntax-tree-surfer",
-		event = "BufRead",
-		config = load_config("syntax_tree_surfer"),
-	},
-	{
-		-- Rainbow delimiters with Treesitter
-		"HiPhish/rainbow-delimiters.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		event = { "BufReadPost", "BufNewFile" },
-		config = load_config("rainbow"),
-	},
+    {
+        -- Generate comments based on treesitter.
+        "danymat/neogen",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        opts = overrides.neogen,
+        -- Uncomment next line if you want to follow only stable versions
+        version = "*",
+        event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = overrides.treesitter,
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = {
+            "TSInstall",
+            "TSInstallSync",
+            "TSInstallInfo",
+            "TSUpdate",
+            "TSUpdateSync",
+            "TSUninstall",
+        },
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-refactor",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "syntax")
+            dofile(vim.g.base46_cache .. "treesitter")
+            require("nvim-treesitter.configs").setup(opts)
+            require("nvim-treesitter.install").compilers = { "clang" }
+        end,
+    },
+    {
+        -- Show code context.,
+        "nvim-treesitter/nvim-treesitter-context",
+        lazy = false,
+        event = "BufReadPost",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = overrides.treesitter_context,
+    },
+    {
+        -- Syntax aware text-objects, select, move, swap, and peek support.
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = {
+            -- "nvim-treesitter/nvim-treesitter",
+        },
+        config = load_config("ts_textobjects"),
+    },
+    {
+        -- Helps you navigate and move nodes around based on Treesitter API.
+        "ziontee113/syntax-tree-surfer",
+        event = "BufRead",
+        config = load_config("syntax_tree_surfer"),
+    },
+    {
+        -- Rainbow delimiters with Treesitter
+        "HiPhish/rainbow-delimiters.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        event = { "BufReadPost", "BufNewFile" },
+        config = load_config("rainbow"),
+    },
 	{
 		-- Lightweight formatter plugin
 		"stevearc/conform.nvim",
@@ -141,189 +141,189 @@ local plugins = {
 		config = load_config("conform"),
 	},
 
-	--#endregion
+    --#endregion
 
-	{
-		"CRAG666/code_runner.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = overrides.code_runner,
-		event = "BufEnter",
-	},
+    {
+        "CRAG666/code_runner.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = overrides.code_runner,
+        event = "BufEnter",
+    },
 
-	--#endregion
+    --#endregion
 
-	--#region Snippets + completion
+    --#region Snippets + completion
 
-	-- h/t https://gist.github.com/ianchesal/93ba7897f81618ca79af01bc413d0713
-	{
-		"hrsh7th/nvim-cmp",
-		opts = overrides.cmp,
-		event = { "InsertEnter", "CmdlineEnter" },
-		config = function(_, opts)
-	require("cmp").setup(opts)
-	load_config("cmp")()
-		end,
-		dependencies = {
-			{
-				-- Completion for LaTeX symbols
-				"amarakon/nvim-cmp-lua-latex-symbols",
-				event = "InsertEnter",
-				opts = { cache = true },
-				-- Necessary to avoid runtime errors
-				config = function(_, opts)
-	require("cmp").setup(opts)
-				end,
-			},
-			{
-				-- Completion for fs paths (async)
-				"FelipeLema/cmp-async-path",
-				url = "https://codeberg.org/FelipeLema/cmp-async-path",
-			},
-			{
-				-- nvim-cmp source for emojis
-				"hrsh7th/cmp-emoji",
-				event = "InsertEnter",
-				enabled = false,
-			},
-			{
-				-- Completion for Neovim's Lua runtime API.
-				"hrsh7th/cmp-nvim-lua",
-				event = "InsertEnter",
-				-- Not needed bc of neodev
-				enabled = false,
-			},
-			{
-				-- Fuzzy buffer completion
-				"tzachar/cmp-fuzzy-buffer",
-				dependencies = { "tzachar/fuzzy.nvim" },
-			},
-			{
-				-- nvim-cmp source for cmdline
-				"hrsh7th/cmp-cmdline",
-				lazy = false,
-			},
-			{
-				-- Luasnip choice node completion source for nvim-cmp
-				"L3MON4D3/cmp-luasnip-choice",
-				config = function()
-	require("cmp_luasnip_choice").setup({
-		auto_open = true, -- Automatically open nvim-cmp on choice node (default: true)
-	})
-				end,
-			},
-			{ "amarakon/nvim-cmp-buffer-lines" },
-			{ "L3MON4D3/LuaSnip" },
-			{ "ray-x/cmp-treesitter" },
-			{ "jalvesaq/cmp-nvim-r" },
-			{ "rcarriga/cmp-dap" },
-			{
-				-- Completion plugin for git
-				"petertriho/cmp-git",
-				dependencies = { "nvim-lua/plenary.nvim" },
-				config = load_config("cmp_git"),
-			},
-		},
-	},
-	{
-		-- Snippet Engine for Neovim
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			{ "rafamadriz/friendly-snippets" },
-			{ "onsails/lspkind.nvim" },
-		},
-		event = {
-			"InsertEnter",
-			"CmdlineEnter",
-		},
-		build = "make install_jsregexp",
-		config = load_config("ls"),
-	},
-	{
-		"allaman/emoji.nvim",
-		--   ft = "markdown", -- adjust to your needs
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-			"nvim-telescope/telescope.nvim",
-		},
-		opts = {
-			-- default is false
-			enable_cmp_integration = true,
-		},
-	},
+    -- h/t https://gist.github.com/ianchesal/93ba7897f81618ca79af01bc413d0713
+    {
+        "hrsh7th/nvim-cmp",
+        opts = overrides.cmp,
+        event = { "InsertEnter", "CmdlineEnter" },
+        config = function(_, opts)
+            require("cmp").setup(opts)
+            load_config("cmp")()
+        end,
+        dependencies = {
+            {
+                -- Completion for LaTeX symbols
+                "amarakon/nvim-cmp-lua-latex-symbols",
+                event = "InsertEnter",
+                opts = { cache = true },
+                -- Necessary to avoid runtime errors
+                config = function(_, opts)
+                    require("cmp").setup(opts)
+                end,
+            },
+            {
+                -- Completion for fs paths (async)
+                "FelipeLema/cmp-async-path",
+                url = "https://codeberg.org/FelipeLema/cmp-async-path",
+            },
+            {
+                -- nvim-cmp source for emojis
+                "hrsh7th/cmp-emoji",
+                event = "InsertEnter",
+                enabled = false,
+            },
+            {
+                -- Completion for Neovim's Lua runtime API.
+                "hrsh7th/cmp-nvim-lua",
+                event = "InsertEnter",
+                -- Not needed bc of neodev
+                enabled = false,
+            },
+            {
+                -- Fuzzy buffer completion
+                "tzachar/cmp-fuzzy-buffer",
+                dependencies = { "tzachar/fuzzy.nvim" },
+            },
+            {
+                -- nvim-cmp source for cmdline
+                "hrsh7th/cmp-cmdline",
+                lazy = false,
+            },
+            {
+                -- Luasnip choice node completion source for nvim-cmp
+                "L3MON4D3/cmp-luasnip-choice",
+                config = function()
+                    require("cmp_luasnip_choice").setup({
+                        auto_open = true, -- Automatically open nvim-cmp on choice node (default: true)
+                    })
+                end,
+            },
+            { "amarakon/nvim-cmp-buffer-lines" },
+            { "L3MON4D3/LuaSnip" },
+            { "ray-x/cmp-treesitter" },
+            { "jalvesaq/cmp-nvim-r" },
+            { "rcarriga/cmp-dap" },
+            {
+                -- Completion plugin for git
+                "petertriho/cmp-git",
+                dependencies = { "nvim-lua/plenary.nvim" },
+                config = load_config("cmp_git"),
+            },
+        },
+    },
+    {
+        -- Snippet Engine for Neovim
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            { "rafamadriz/friendly-snippets" },
+            { "onsails/lspkind.nvim" },
+        },
+        event = {
+            "InsertEnter",
+            "CmdlineEnter",
+        },
+        build = "make install_jsregexp",
+        config = load_config("ls"),
+    },
+    {
+        "allaman/emoji.nvim",
+        --   ft = "markdown", -- adjust to your needs
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "nvim-telescope/telescope.nvim",
+        },
+        opts = {
+            -- default is false
+            enable_cmp_integration = true,
+        },
+    },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		-- if you load some function or module within your opt, wrap it with a function
-		opts = function()
-	local conf = require("nvchad.configs.telescope")
-	conf.defaults.mappings.i = {
-		["<C-j>"] = require("telescope.actions").move_selection_next,
-		["<esc>"] = require("telescope.actions").close,
-	}
-	conf.extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		},
-	}
-	return conf
-		end,
-		-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
-		dependencies = {
-			{
-				-- Use fzf syntax in Telescope.
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-			},
-			"benfowler/telescope-luasnip.nvim",
-			"tom-anders/telescope-vim-bookmarks.nvim",
-			"debugloop/telescope-undo.nvim",
-			"nvim-telescope/telescope-frecency.nvim",
-			"catgoose/telescope-helpgrep.nvim",
-			-- "stevearc/aerial.nvim",
-		},
-		config = function(_, opts)
-	dofile(vim.g.base46_cache .. "telescope")
-	local telescope = require("telescope")
-	telescope.setup(opts)
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        -- if you load some function or module within your opt, wrap it with a function
+        opts = function()
+            local conf = require("nvchad.configs.telescope")
+            conf.defaults.mappings.i = {
+                ["<C-j>"] = require("telescope.actions").move_selection_next,
+                ["<esc>"] = require("telescope.actions").close,
+            }
+            conf.extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+            }
+            return conf
+        end,
+        -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+        dependencies = {
+            {
+                -- Use fzf syntax in Telescope.
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            },
+            "benfowler/telescope-luasnip.nvim",
+            "tom-anders/telescope-vim-bookmarks.nvim",
+            "debugloop/telescope-undo.nvim",
+            "nvim-telescope/telescope-frecency.nvim",
+            "catgoose/telescope-helpgrep.nvim",
+            -- "stevearc/aerial.nvim",
+        },
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "telescope")
+            local telescope = require("telescope")
+            telescope.setup(opts)
 
-	telescope.load_extension("fzf")
-	telescope.load_extension("emoji")
-	telescope.load_extension("themes")
-	telescope.load_extension("terms")
-	telescope.load_extension("notify")
-	telescope.load_extension("luasnip")
-	telescope.load_extension("undo")
-	telescope.load_extension("helpgrep")
-	telescope.load_extension("file_browser")
-			-- telescope.load_extension("aerial")
-		end,
-	},
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	},
-	{
-		"axieax/urlview.nvim",
-		cmd = { "UrlView" },
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"stevearc/dressing.nvim",
-		},
-		opts = {
-			default_title = "URLs",
-			default_picker = "telescope",
-			default_action = "clipboard",
-			sorted = false,
-			jump = {
-				["prev"] = "[u",
-				["next"] = "]u",
-			},
-		},
-	},
+            telescope.load_extension("fzf")
+            telescope.load_extension("emoji")
+            telescope.load_extension("themes")
+            telescope.load_extension("terms")
+            telescope.load_extension("notify")
+            telescope.load_extension("luasnip")
+            telescope.load_extension("undo")
+            telescope.load_extension("helpgrep")
+            telescope.load_extension("file_browser")
+            -- telescope.load_extension("aerial")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    },
+    {
+        "axieax/urlview.nvim",
+        cmd = { "UrlView" },
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "stevearc/dressing.nvim",
+        },
+        opts = {
+            default_title = "URLs",
+            default_picker = "telescope",
+            default_action = "clipboard",
+            sorted = false,
+            jump = {
+                ["prev"] = "[u",
+                ["next"] = "]u",
+            },
+        },
+    },
 
 	-- Haskell
 	{
@@ -468,27 +468,27 @@ local plugins = {
 		-- <C-e>: Toggles visually selected text to inline code, and V-LINE selected text to a multiline code block.
 	},
 
-	-- Appearance
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
-		-- Setting these in config was not working 😦
-		init = function()
-	vim.opt.fillchars = {
-		fold = " ",
-		foldopen = "",
-		foldsep = " ",
-		foldclose = "",
-		stl = " ",
-		eob = " ",
-	}
-	vim.o.foldcolumn = "1"
-	vim.o.foldenable = true -- enable fold for nvim-ufo
-	vim.o.foldlevel = 99 -- set high foldlevel for nvim-ufo
-	vim.o.foldlevelstart = 99 -- start with all code unfolded
-		end,
-		config = load_config("ufo"),
-	},
+    -- Appearance
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter" },
+        -- Setting these in config was not working 😦
+        init = function()
+            vim.opt.fillchars = {
+                fold = " ",
+                foldopen = "",
+                foldsep = " ",
+                foldclose = "",
+                stl = " ",
+                eob = " ",
+            }
+            vim.o.foldcolumn = "1"
+            vim.o.foldenable = true -- enable fold for nvim-ufo
+            vim.o.foldlevel = 99 -- set high foldlevel for nvim-ufo
+            vim.o.foldlevelstart = 99 -- start with all code unfolded
+        end,
+        config = load_config("ufo"),
+    },
 }
 
 return plugins
