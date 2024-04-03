b85495b5ecdbeaa46e18ad4cedfff7cce73c799d
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-04-02 18:31:45 -0700

    :recycle: Clean up PS LSP config.

--- home/dot_config/nvim/lua/configs/lspconfig.lua
+++ home/dot_config/nvim/lua/configs/lspconfig.lua
@@ -185,7 +185,35 @@ lspconfig.yamlls.setup({
     },
 })
 
-require("configs.powershell")
+-- https://github.com/PowerShell/PowerShellEditorServices/blob/89ce0867c6b119bef8af83ab21c249e10d0e77a2/docs/guide/getting_started.md
+
+local home_directory = os.getenv("HOME")
+if home_directory == nil then
+    home_directory = os.getenv("USERPROFILE")
+end
+
+-- The bundle_path is where PowerShell Editor Services was installed
+local bundle_path = home_directory .. "/PowerShellEditorServices"
+
+lspconfig.powershell_es.setup({
+    bundle_path = bundle_path,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        powershell = {
+            codeFormatting = {
+                Preset = "OTBS",
+                AddWhitespaceAroundPipe = true,
+                IgnoreOneLineBlock = true,
+                PipelineIndentationStyle = "IncreaseIndentationForFirstPipeline",
+                TrimWhitespaceAroundPipe = true,
+                UseConstantStrings = true,
+                UseCorrectCasing = true,
+                WhitespaceBetweenParameters = true,
+            },
+        },
+    },
+})
 
 lspconfig.basedpyright.setup({
     on_attach = on_attach,
