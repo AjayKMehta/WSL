cf1914188486c1f9a6452389ad8e6bd28cf7f7da
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-04-02 17:53:08 -0700

    Add highlight-undo

--- home/dot_config/nvim/lua/plugins/myplugins.lua
+++ home/dot_config/nvim/lua/plugins/myplugins.lua
@@ -407,6 +407,29 @@ local plugins = {
         end,
         event = "VeryLazy",
     },
+    {
+        -- highlight-undo will remap the u and <C-r> keys (for undo and redo, by default) to highlight changed text after Undo / Redo operations.
+        "tzachar/highlight-undo.nvim",
+        opts = {
+            duration = 300,
+            undo = {
+                hlgroup = "HighlightUndo",
+                mode = "n",
+                lhs = "u",
+                map = "undo",
+                opts = {},
+            },
+            redo = {
+                hlgroup = "HighlightUndo",
+                mode = "n",
+                lhs = "<C-r>",
+                map = "redo",
+                opts = {},
+            },
+            highlight_for_count = true,
+        },
+        event = { "BufReadPre" },
+    },
 
     -- Appearance
     {
