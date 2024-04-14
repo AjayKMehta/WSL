964852a70b995ebc8618cdcfc0eda5bb61bc9ae2
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-04-14 08:09:34 -0700

    New file for comment-related plugins.

--- home/dot_config/nvim/lua/plugins/myplugins.lua
+++ home/dot_config/nvim/lua/plugins/myplugins.lua
@@ -36,56 +36,6 @@ local plugins = {
         "folke/flash.nvim",
         event = "VeryLazy",
     },
-
-    {
-        -- Advanced comment plugin with Treesitter support.
-        "numToStr/Comment.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        config = function(_, opts)
-            local comment = require("Comment")
-            -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#commentnvim
-            local pre_hook = {
-                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-            }
-            comment.setup(vim.tbl_deep_extend("force", opts, pre_hook))
-        end,
-        dependencies = {
-            {
-                "JoosepAlviste/nvim-ts-context-commentstring",
-                lazy = false,
-                config = function()
-                    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
-                    require("ts_context_commentstring").setup({
-                        enable_autocmd = false,
-                    })
-                    vim.g.skip_ts_context_commentstring_module = true
-                end,
-            },
-        },
-    },
-
-    {
-        -- Generate comments based on treesitter.
-        "danymat/neogen",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        opts = {
-            enabled = true,
-            snippet_engine = "luasnip",
-            languages = {
-                lua = {
-                    template = {
-                        annotation_convention = "emmylua",
-                    },
-                    -- TODO: Figure out how to fix.
-                    cs = { template = { annotation_convention = "xmldoc" } },
-                },
-            },
-        },
-        -- Uncomment next line if you want to follow only stable versions
-        version = "*",
-        event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-    },
     {
         "nvim-treesitter/nvim-treesitter",
         opts = overrides.treesitter,
