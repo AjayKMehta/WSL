d45f3a091dd53744a8422d58e862a51b681c266b
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-05-17 21:25:36 -0700

    Fix descriptions so mappings show up in cheatsheet properly.

--- home/dot_config/nvim/lua/utils/lsp.lua
+++ home/dot_config/nvim/lua/utils/lsp.lua
@@ -32,30 +32,28 @@ end
 M.on_attach = function(client, bufnr)
     local map_buf = require("utils.mappings").map_buf
 
-    map_buf(bufnr, "n", "<leader>lD", vim.lsp.buf.declaration, "Lsp Go to declaration")
+    map_buf(bufnr, "n", "<leader>lD", vim.lsp.buf.declaration, "LSP Go to declaration")
 
-    map_buf(bufnr, "n", "<leader>ld", vim.lsp.buf.definition, "Lsp Go to definition")
-    map_buf(bufnr, "n", "<F12>", vim.lsp.buf.definition, "Lsp Go to definition")
+    map_buf(bufnr, "n", "<leader>ld", vim.lsp.buf.definition, "LSP Go to definition")
+    map_buf(bufnr, "n", "<F12>", vim.lsp.buf.definition, "LSP Go to definition")
 
-    map_buf(bufnr, "n", "<leader>lr", vim.lsp.buf.references, "Lsp Show references")
+    map_buf(bufnr, "n", "<leader>lr", vim.lsp.buf.references, "LSP Show references")
 
-    map_buf(bufnr, "n", "<leader>lT", vim.lsp.buf.type_definition, "Lsp Go to type definition")
+    map_buf(bufnr, "n", "<leader>lT", vim.lsp.buf.type_definition, "LSP Go to type definition")
 
-    map_buf(bufnr, "n", "K", hover_handler, "Lsp hover information")
+    map_buf(bufnr, "n", "<leader>li", vim.lsp.buf.implementation, "LSP Go to implementation")
 
-    map_buf(bufnr, "n", "<leader>li", vim.lsp.buf.implementation, "Lsp Go to implementation")
+    map_buf(bufnr, "n", "<leader>lh", vim.lsp.buf.signature_help, "LSP Show signature help")
 
-    map_buf(bufnr, "n", "<leader>lh", vim.lsp.buf.signature_help, "Lsp Show signature help")
-
-    map_buf(bufnr, "n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Lsp Add workspace folder")
-    map_buf(bufnr, "n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Lsp Remove workspace folder")
+    map_buf(bufnr, "n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "LSP Add workspace folder")
+    map_buf(bufnr, "n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "LSP Remove workspace folder")
     map_buf(bufnr, "n", "<leader>wl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "Lsp List workspace folders")
+    end, "LSP List workspace folders")
 
     map_buf(bufnr, "n", "<leader>ra", function()
         require("nvchad.lsp.renamer")()
-    end, "Lsp NvRenamer")
+    end, "LSP NvRenamer")
 
     map_buf({ "n", "v" }, "<leader>lc", vim.lsp.buf.code_action, "Lsp Code action")
 
