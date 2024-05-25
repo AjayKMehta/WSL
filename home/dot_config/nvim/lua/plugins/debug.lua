31b7e67770c0e4492c42ba364a186d26214ab16b
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-05-24 17:56:06 -0700

    Fix DAP UI. Disable PowerShell DAP.

--- home/dot_config/nvim/lua/plugins/debug.lua
+++ home/dot_config/nvim/lua/plugins/debug.lua
@@ -2,25 +2,23 @@ local load_config = require("utils").load_config
 
 return {
     {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        config = load_config("dap_ui"),
-    },
-    {
         "mfussenegger/nvim-dap",
         config = load_config("dap"),
-    },
-    {
-        "mfussenegger/nvim-dap-python",
-        ft = "python",
         dependencies = {
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
-        },
-        config = function(_, opts)
-            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-            require("dap-python").setup(path)
-        end,
+            {
+                "rcarriga/nvim-dap-ui",
+                dependencies = {  "nvim-neotest/nvim-nio" },
+            },
+            {
+                "mfussenegger/nvim-dap-python",
+                ft = "python",
+                dependencies = { "HiPhish/debugpy.nvim" },
+                config = function(_, opts)
+                    local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+                    require("dap-python").setup(path)
+                end,
+            },
+        }
     },
     {
         -- It's important that you set up the plugins in the following order:
@@ -38,15 +36,21 @@ return {
         end,
     },
     {
-        "Willem-J-an/nvim-dap-powershell",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
+    "Willem-J-an/nvim-dap-powershell",
+    -- TODO: Get this to work.
+    enabled = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "mfussenegger/nvim-dap",
+        "rcarriga/nvim-dap-ui",
+        {
             "m00qek/baleia.nvim",
+            lazy = true,
+            tag = "v1.4.0",
         },
-        config = function()
-            require("dap-powershell").setup()
-        end,
     },
+    config = function()
+        require("dap-powershell").setup()
+    end,
+}
 }
