310180381f59e66609460a94457cc66bea35f184
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-05-24 17:54:29 -0700

    Merge dap_ui config with dap's.

--- home/dot_config/nvim/lua/configs/dap.lua
+++ home/dot_config/nvim/lua/configs/dap.lua
@@ -4,6 +4,18 @@
 -- Make sure to also have the snippet with the common helper functions in your config!
 
 local dap = require("dap")
+local dapui = require("dapui")
+
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
 
 -- Setup icons
 -- vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
