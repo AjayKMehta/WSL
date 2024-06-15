43ff9a71e4919d1ad7898a08ce07af4ddb91d8ed
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-06-15 12:50:34 -0700

    Change mergetool settings for diffview.

--- home/dot_config/nvim/lua/configs/diffview.lua
+++ home/dot_config/nvim/lua/configs/diffview.lua
@@ -2,6 +2,14 @@
 local actions = require("diffview.actions")
 require("diffview").setup({
     enhanced_diff_hl = true,
+    view = {
+        merge_tool = {
+            -- Config for conflicted files in diff views during a merge or rebase.
+            layout = "diff4_mixed", -- default is diff3_horizontal
+            disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+            winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+        },
+    },
     keymaps = {
         file_panel = {
             {
