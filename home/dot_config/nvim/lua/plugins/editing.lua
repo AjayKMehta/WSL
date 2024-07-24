fe371453dc9781a16d67c243dc00123e3e39d56f
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-07-24 15:57:44 -0700

    Add precognition.nvim

--- home/dot_config/nvim/lua/plugins/editing.lua
+++ home/dot_config/nvim/lua/plugins/editing.lua
@@ -166,4 +166,74 @@ return {
             prefix = "possible misspelling(s): ", -- prefix for each diagnostic message
         },
     },
+    -- Assists with discovering motions
+    {
+        "tris203/precognition.nvim",
+        enabled = true,
+        event = { "BufRead", "BufNewFile" },
+        cmd = { "Precognition" },
+        keys = {
+            {
+                "<leader>pT",
+                function()
+                    if require("precognition").toggle() then
+                        vim.notify("precognition on")
+                    else
+                        vim.notify("precognition off")
+                    end
+                end,
+                { desc = "Precognition toggle" },
+            },
+            {
+                "<leader>pp",
+                "<cmd>Precognition peek<CR>",
+                { desc = "Precognition peek" },
+            },
+        },
+        opts = {
+            startVisible = true,
+            showBlankVirtLine = true,
+            highlightColor = { link = "Comment" },
+            hints = {
+                Caret = { text = "^", prio = 2 },
+                Dollar = { text = "$", prio = 1 },
+                MatchingPair = { text = "%", prio = 5 },
+                Zero = { text = "0", prio = 1 },
+                w = { text = "w", prio = 10 },
+                b = { text = "b", prio = 9 },
+                e = { text = "e", prio = 8 },
+                W = { text = "W", prio = 7 },
+                B = { text = "B", prio = 6 },
+                E = { text = "E", prio = 5 },
+            },
+            gutterHints = {
+                G = { text = "G", prio = 10 },
+                gg = { text = "gg", prio = 9 },
+                PrevParagraph = { text = "{", prio = 8 },
+                NextParagraph = { text = "}", prio = 8 },
+            },
+            disabled_fts = {
+                "aerial",
+                "checkhealth",
+                "cmp_menu",
+                "flash_prompt",
+                "lazy",
+                "help",
+                "man",
+                "mason",
+                "noice",
+                "notify",
+                "NvimTree",
+                "startify",
+                "oil",
+                "Outline",
+                "qf",
+                "startuptime",
+                "TelescopePrompt",
+                "toggleterm",
+                "dropbar_menu",
+                "Trouble",
+            },
+        },
+    },
 }
