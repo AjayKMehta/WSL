05c24fd4b87275afc4c50d359a6c42cc13e89783
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-09-19 07:54:28 -0700

    Update render-markdown config.

--- home/dot_config/nvim/lua/plugins/markdown.lua
+++ home/dot_config/nvim/lua/plugins/markdown.lua
@@ -89,6 +89,18 @@ return {
         opts = {
             file_types = { "markdown", "quarto", "rmd", "org", "norg" },
             quote = { repeat_linebreak = true },
+            -- Mimic org-indent-mode behavior by indenting everything under a heading based on the
+            -- level of the heading. Indenting starts from level 2 headings onward.
+            indent = {
+                enabled = true,
+                -- Amount of additional padding added for each heading level
+                per_level = 2,
+                -- Heading levels <= this value will not be indented
+                -- Use 0 to begin indenting from the very first level
+                skip_level = 1,
+                -- Do not indent heading titles, only the body
+                skip_heading = false,
+            },
             win_options = {
                 showbreak = { default = "", rendered = "  " },
                 breakindent = { default = false, rendered = true },
