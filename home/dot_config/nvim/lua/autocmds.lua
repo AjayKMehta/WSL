6b0f03b36f5a6ed31adf7df1d191819112eeedf4
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-12-07 16:39:19 -0800

    Autocmd for roslyn diagnostic refresh.

--- home/dot_config/nvim/lua/autocmds.lua
+++ home/dot_config/nvim/lua/autocmds.lua
@@ -177,3 +177,20 @@ autocmd("User", {
         end)
     end,
 })
+
+
+-- https://github.com/seblj/roslyn.nvim/wiki#diagnostic-refresh
+autocmd({ "InsertLeave" }, {
+    pattern = "*",
+    callback = function()
+        local clients = vim.lsp.get_clients({ name = "roslyn" })
+        if not clients or #clients == 0 then
+            return
+        end
+
+        local buffers = vim.lsp.get_buffers_by_client_id(clients[1].id)
+        for _, buf in ipairs(buffers) do
+            vim.lsp.util._refresh("textDocument/diagnostic", { bufnr = buf })
+        end
+    end,
+})
