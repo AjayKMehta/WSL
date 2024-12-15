736d6e3628443728aa8e9dd73e7be6478a880e88
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-12-14 19:51:50 -0800

    Update vimtex-based keymaps for tex files.

--- home/dot_config/nvim/ftplugin/tex.lua
+++ home/dot_config/nvim/ftplugin/tex.lua
@@ -1,7 +1,10 @@
-local map_buf= require("utils.mappings").map_buf
+local map_buf = require("utils.mappings").map_buf
 
 -- Necessary because `]m`, etc. used by treesitter to move to method start/end
-map_buf(0, "n", "[e", "<plug>(vimtex-[m)", "Go to start of current environment")
-map_buf(0, "n", "]e", "<plug>(vimtex-]m)", "Go to start of next environment")
-map_buf(0, "n", "[E", "<plug>(vimtex-[M)", "Go to end of previous environment")
-map_buf(0, "n", "]E", "<plug>(vimtex-]M)", "Go to end of current environment")
+map_buf(0, { "n", "x", "o" }, "[e", "<plug>(vimtex-[m)", "Go to start of current environment")
+map_buf(0, { "n", "x", "o" }, "]e", "<plug>(vimtex-]m)", "Go to start of next environment")
+map_buf(0, { "n", "x", "o" }, "[E", "<plug>(vimtex-[M)", "Go to end of previous environment")
+map_buf(0, { "n", "x", "o" }, "]E", "<plug>(vimtex-]M)", "Go to end of current environment")
+
+map_buf(0, { "x", "o" }, "ic", "<plug>(vimtex-targets-i)c", "Inner command")
+map_buf(0, { "x", "o" }, "ac", "<plug>(vimtex-targets-a)c", "Around command")
