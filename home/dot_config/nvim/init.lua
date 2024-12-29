8f71180948bc8a96029fbe4756a48035a4579ced
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-12-28 18:01:51 -0800

    Add toggle for LSP inline hints.

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -35,6 +35,10 @@ vim.g.diffview_base46 = true
 -- if true, use zathura_simple for viewing PDFs with vimtex
 vim.g.use_zathura_simple = true
 
+-- if true, use lsp_lines.nvim instead of nvim-lsp-endhints for displaying LSP
+-- diagnostics
+vim.g.lsp_inline = true
+
 -- load plugins
 require("lazy").setup({
     {
--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -16,15 +16,24 @@ return {
         -- lsp_lines is a simple neovim plugin that renders diagnostics using virtual lines on top of the real line of code.
         url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
         dependencies = { "neovim/nvim-lspconfig" },
+        event = { "LspAttach" },
+        enabled = vim.g.lsp_inline,
         config = function()
+            -- Disable virtual_text since it's redundant due to lsp_lines.
+            vim.diagnostic.config({
+                virtual_text = false,
+            })
             local lsp_lines = require("lsp_lines")
             lsp_lines.setup()
-            lsp_lines.toggle()
         end,
         keys = {
             {
                 "<leader>ltd",
                 function()
+                    local text_enabled = vim.diagnostic.config().virtual_text
+                    vim.diagnostic.config({
+                        virtual_text = not text_enabled,
+                    })
                     require("lsp_lines").toggle()
                 end,
                 desc = "Lsp Toggle inline diagnostics",
@@ -44,6 +53,7 @@ return {
         -- Display LSP inlay hints at the end of the line, rather than within the line.
         "chrisgrieser/nvim-lsp-endhints",
         event = "LspAttach",
+        enabled = not vim.g.lsp_inline,
         opts = {}, -- required, even if empty
         keys = {
             {
