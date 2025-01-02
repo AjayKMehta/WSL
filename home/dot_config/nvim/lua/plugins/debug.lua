5d890e5c927a9c44483af7574cb73b1a7489c419
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-01-01 16:59:53 -0800

    Fix config for nvim-dap-virtual-text.

--- home/dot_config/nvim/lua/plugins/debug.lua
+++ home/dot_config/nvim/lua/plugins/debug.lua
@@ -2,37 +2,59 @@ local load_config = require("utils").load_config
 
 return {
     {
-        "mfussenegger/nvim-dap",
-        config = load_config("dap"),
-        dependencies = {
-            {
-                "rcarriga/nvim-dap-ui",
-                dependencies = { "nvim-neotest/nvim-nio" },
-            },
-            {
-                "theHamsta/nvim-dap-virtual-text",
-                opts = {
-                    enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-                    highlight_changed_variables = true,
-                    show_stop_reason = true, -- show stop reason when stopped for exceptions
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        {
+            "mfussenegger/nvim-dap",
+            config = load_config("dap"),
+            dependencies = {
+                {
+                    "theHamsta/nvim-dap-virtual-text",
+                    opts = {
+                        enabled = true, -- enable this plugin (the default)
+                        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+                        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                        show_stop_reason = true, -- show stop reason when stopped for exceptions
+                        commented = true, -- prefix virtual text with comment string (otherwise a bit confusing IMO)
+                        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+                        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+                        clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+                        --- A callback that determines how a variable is displayed or whether it should be omitted
+                        display_callback = function(variable, buf, stackframe, node, options)
+                            if options.virt_text_pos == "inline" then
+                                return " = " .. variable.value
+                            else
+                                return variable.name .. " = " .. variable.value
+                            end
+                        end,
+                        -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+                        virt_text_pos = "inline",
+
+                        -- experimental features:
+                        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                        virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+                        virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+                    },
+                    config = function(_, opts)
+                        require("nvim-dap-virtual-text").setup(opts)
+                        vim.api.nvim_create_user_command("DapVirtualTextClear", function()
+                            require("nvim-dap-virtual-text.virtual_text").clear_virtual_text()
+                        end, {
+                            desc = "Clear all the virtual text displayed by nvim-dap-virtual-text",
+                            force = true,
+                        })
+                    end,
                 },
-                config = function(_, opts)
-                    require("nvim-dap-virtual-text").setup(opts)
-                    vim.api.nvim_create_user_command("DapVirtualTextClear", function()
-                        require("nvim-dap-virtual-text.virtual_text").clear_virtual_text()
-                    end, {
-                        desc = "Clear all the virtual text displayed by nvim-dap-virtual-text",
-                        force = true,
-                    })
-                end,
-            },
-            {
-                "mfussenegger/nvim-dap-python",
-                ft = "python",
-                dependencies = { "HiPhish/debugpy.nvim" },
-                config = function(_, opts)
-                    require("dap-python").setup("uv")
-                end,
+                {
+                    "mfussenegger/nvim-dap-python",
+                    ft = "python",
+                    dependencies = { "HiPhish/debugpy.nvim" },
+                    config = function(_, opts)
+                        require("dap-python").test_runner = "pytest"
+                        require("dap-python").setup("uv")
+                    end,
+                },
             },
         },
     },
