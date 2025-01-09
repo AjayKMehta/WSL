a2ea1aad3d644b2342a5995fc0558df695bea876
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-01-08 18:24:23 -0800

    Update telescope-helpgrep mappings.

--- home/dot_config/nvim/lua/configs/telescope.lua
+++ home/dot_config/nvim/lua/configs/telescope.lua
@@ -136,6 +136,21 @@ M.config = function()
                 override_file_sorter = true,
                 case_mode = "smart_case",
             },
+            helpgrep = {
+                ignore_paths = {
+                    vim.fn.stdpath("state") .. "/lazy/readme",
+                },
+                mappings = {
+                    i = {
+                        ["<CR>"] = actions.select_default,
+                        ["<C-v>"] = actions.select_vertical,
+                    },
+                    n = {
+                        ["<CR>"] = actions.select_default,
+                        ["<C-s>"] = actions.select_horizontal,
+                    },
+                },
+            },
             lazy = {
                 show_icon = true,
                 mappings = {
