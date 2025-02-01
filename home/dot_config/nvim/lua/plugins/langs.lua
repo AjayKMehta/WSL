493e5a1f5512a00804102666d881f019ab7ceb62
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-01-31 19:26:23 -0800

    Add boilersharp.nvim.

--- home/dot_config/nvim/lua/plugins/langs.lua
+++ home/dot_config/nvim/lua/plugins/langs.lua
@@ -207,4 +207,65 @@ return {
             require("powershell").setup(opts)
         end,
     },
+    {
+        --- Automatically generate C# boilerplate when you create a file inside a C# project
+         "DestopLine/boilersharp.nvim",
+        opts = {
+            usings = {
+                ---@type "never" | "always" | "auto"
+                implicit_usings = "never",
+                ---@type string[]
+                usings = {
+                    "System",
+                    "System.Collections.Generic",
+                    "System.IO",
+                    "System.Linq",
+                    "System.Net.Http",
+                    "System.Threading",
+                    "System.Threading.Tasks",
+                },
+            },
+
+            namespace = {
+                ---When to use file scoped namespace syntax.
+                ---Set this to "auto" to get this from the C# version inferred from
+                ---the csproj file of the project.
+                ---@type "never" | "always" | "auto"
+                use_file_scoped = "always",
+            },
+
+            type_declaration = {
+                ---Access modifier to use when writing boilerplate. Set this to
+                ---`false` to not use any access modifier (implicitly `internal`).
+                default_access_modifier = "public",
+
+                ---C# keyword to use when declaring the type.
+                default_type_declaration = "class",
+
+                ---Whether the plugin should use the `interface` keyword for the
+                ---type declaration when the name of the type matches the C#
+                ---interface naming convention, which would be equivalent to this
+                ---regular expression: `^I[A-Z].*$`.
+                infer_interfaces = true,
+            },
+
+            ---Whether to add autocommands for writing boilerplate when you enter
+            ---an empty C# file. Set this to `false` if you wanna be in control
+            ---of when boilerplate gets written to the file through user commands
+            ---or lua functions.
+            add_autocommand = false,
+
+            ---What type of indentation to use for boilerplate generation. This is
+            ---only ever used when not using file scoped namespace syntax and
+            ---`type_declaration` is enabled. Set this to "auto" to take this from
+            ---the buffer's options.
+            ---
+            ---It is recommended that you set up an "after/ftplugin/cs.lua" file
+            ---in your nvim config with options for `expandtab` instead of
+            ---changing this option from its default value.
+            ---See `:h ftplugin` and `:h after-directory`.
+            ---@type "tabs" | "spaces" | "auto"
+            indent_type = "auto",
+        },
+    },
 }
