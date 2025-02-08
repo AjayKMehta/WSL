a6f8e770c3ed2a85b67a03491be957ccf6d857ff
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-02-07 20:16:51 -0800

    Update keymaps.

--- home/dot_config/nvim/lua/configs/treesitter.lua
+++ home/dot_config/nvim/lua/configs/treesitter.lua
@@ -113,8 +113,8 @@ local config = {
                 -- You can use the capture groups defined in textobjects.scm
                 ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
                 ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-                ["L="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-                ["R="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+                ["=L"] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+                ["=R"] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
                 ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
                 ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
