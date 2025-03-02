0678e86acfff2d26e05c644c466718405e301a87
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-03-02 10:08:26 -0800

    Improve YAML plugins' setup + add keymap to select schema.

--- home/dot_config/nvim/lua/plugins/yaml.lua
+++ home/dot_config/nvim/lua/plugins/yaml.lua
@@ -4,56 +4,66 @@ return {
         dependencies = {
             { "neovim/nvim-lspconfig" },
             { "nvim-lua/plenary.nvim" },
-            { "nvim-telescope/telescope.nvim" },
+        },
+        keys = {
+            {
+                "<leader>ys",
+                function()
+                    require("yaml-companion").open_ui_select()
+                end,
+                desc = "YAML Schema",
+            },
         },
         -- Inspired by https://www.arthurkoziel.com/json-schemas-in-neovim/
-        opts = {
-            builtin_matchers = {
-                -- Detects Kubernetes files based on content
-                kubernetes = { enabled = true },
-                cloud_init = { enabled = true },
-            },
-
-            -- Additional schemas available in Telescope picker
-            schemas = {
-                -- schemas below are automatically loaded, but added
-                -- them here so that they show up in the statusline
-                {
-                    name = "GitHub Workflow",
-                    uri = "https://json.schemastore.org/github-workflow.json",
+        opts = function()
+            local ss_schemas = require("schemastore").yaml.schemas()
+            return {
+                builtin_matchers = {
+                    -- Detects Kubernetes files based on content
+                    kubernetes = { enabled = true },
+                    cloud_init = { enabled = true },
                 },
-            },
 
-            -- Pass any additional options that will be merged in the final LSP config
-            lspconfig = {
-                flags = {
-                    debounce_text_changes = 150,
-                },
-                settings = {
-                    redhat = { telemetry = { enabled = false } },
-                    yaml = {
-                        validate = true,
-                        format = { enable = true },
-                        hover = true,
-                        completion = true,
-                        schemaStore = {
-                            enable = false,
-                            url = "",
-                        },
-                        schemaDownload = { enable = true },
-                        -- Transitive dependency due to lspconfig
-                        schemas = require("schemastore").yaml.schemas(),
-                        trace = { server = "debug" },
+                -- Additional schemas available in Telescope picker
+                schemas = {
+                    -- schemas below are automatically loaded, but added
+                    -- them here so that they show up in the statusline
+                    {
+                        name = "GitHub Workflow",
+                        uri = "https://json.schemastore.org/github-workflow.json",
                     },
                 },
-            },
-        },
+
+                -- Pass any additional options that will be merged in the final LSP config
+                lspconfig = {
+                    flags = {
+                        debounce_text_changes = 150,
+                    },
+                    settings = {
+                        redhat = { telemetry = { enabled = false } },
+                        yaml = {
+                            validate = true,
+                            format = { enable = true },
+                            hover = true,
+                            completion = true,
+                            schemaStore = {
+                                enable = false,
+                                url = "",
+                            },
+                            schemaDownload = { enable = true },
+                            -- Transitive dependency due to lspconfig
+                            schemas = ss_schemas,
+                            trace = { server = "debug" },
+                        },
+                    },
+                },
+            }
+        end,
         config = function(_, opts)
             local cfg = require("yaml-companion").setup(opts)
             local lsp = require("utils.lsp")
             cfg = vim.tbl_deep_extend("force", cfg, { on_attach = lsp.on_attach, capabilities = lsp.capabilities })
             require("lspconfig")["yamlls"].setup(cfg)
-            require("telescope").load_extension("yaml_schema")
         end,
     },
     {
@@ -61,7 +71,6 @@ return {
         ft = { "yaml" }, -- optional
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
-            "nvim-telescope/telescope.nvim", -- optional
         },
     },
 }
