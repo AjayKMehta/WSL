508a0a0b7436c3fa7b446bbd48347f0f805f8055
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-03-05 21:22:47 -0800

    Add keymap to delete surrounding indentation (dsi).

--- home/dot_config/nvim/lua/plugins/editing.lua
+++ home/dot_config/nvim/lua/plugins/editing.lua
@@ -88,12 +88,12 @@ return {
             vim.api.nvim_create_autocmd("CursorMoved", {
                 desc = "Highlight when cursor moved significantly",
                 callback = function()
-                        require("undo-glow").cursor_moved({
-                            animation = {
-                                animation_type = "slide",
-                            },
-                            { "mason", "lazy", "help", "git" },
-                        })
+                    require("undo-glow").cursor_moved({
+                        animation = {
+                            animation_type = "slide",
+                        },
+                        { "mason", "lazy", "help", "git" },
+                    })
                 end,
             })
         end,
@@ -421,6 +421,27 @@ return {
                 r.clear_register(reg)
                 vim.ui.open(url)
             end, { desc = "Open URL (smart)" })
+
+            -- https://github.com/chrisgrieser/nvim-various-textobjs?tab=readme-ov-file#delete-surrounding-indentation
+            vim.keymap.set("n", "dsi", function()
+                -- select outer indentation
+                require("various-textobjs").indentation("outer", "outer")
+
+                -- plugin only switches to visual mode when a textobj has been found
+                local indentationFound = vim.fn.mode():find("V")
+                if not indentationFound then
+                    return
+                end
+
+                -- dedent indentation
+                vim.cmd.normal({ "<", bang = true })
+
+                -- delete surrounding lines
+                local endBorderLn = vim.api.nvim_buf_get_mark(0, ">")[1]
+                local startBorderLn = vim.api.nvim_buf_get_mark(0, "<")[1]
+                vim.cmd(tostring(endBorderLn) .. " delete") -- delete end first so line index is not shifted
+                vim.cmd(tostring(startBorderLn) .. " delete")
+            end, { desc = "Delete Surrounding Indentation" })
         end,
     },
 }
