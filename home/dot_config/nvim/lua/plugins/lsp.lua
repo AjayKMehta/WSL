697b1c610cd6fd0faf64d157df8511f932885598
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-03-30 20:53:27 -0700

    Add lsp-endhints config.

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -29,5 +29,26 @@ return {
                 desc = "Lsp Toggle hints at end of line.",
             },
         },
+        config = function()
+            -- default settings
+            require("lsp-endhints").setup({
+                icons = {
+                    type = "󰜁 ",
+                    parameter = "󰏪 ",
+                    offspec = " ", -- hint kind not defined in official LSP spec
+                    unknown = " ", -- hint kind is nil
+                },
+                label = {
+                    truncateAtChars = 20,
+                    padding = 1,
+                    marginLeft = 0,
+                    sameKindSeparator = ", ",
+                },
+                extmark = {
+                    priority = 50,
+                },
+                autoEnableHints = true,
+            })
+        end,
     },
 }
