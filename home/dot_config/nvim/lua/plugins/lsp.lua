77f54c1f8e9076c1d108d93dd7c794b25ed32a68
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-03-31 16:56:55 -0700

    Remove plugin nvim-lsp-endhints.

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -15,40 +15,4 @@ return {
             create_cmds = true, -- Whether to create user commands
         },
     },
-    {
-        -- Display LSP inlay hints at the end of the line, rather than within the line.
-        "chrisgrieser/nvim-lsp-endhints",
-        event = "LspAttach",
-        opts = {}, -- required, even if empty
-        keys = {
-            {
-                "<leader>lte",
-                function()
-                    require("lsp-endhints").toggle()
-                end,
-                desc = "Lsp Toggle hints at end of line.",
-            },
-        },
-        config = function()
-            -- default settings
-            require("lsp-endhints").setup({
-                icons = {
-                    type = "󰜁 ",
-                    parameter = "󰏪 ",
-                    offspec = " ", -- hint kind not defined in official LSP spec
-                    unknown = " ", -- hint kind is nil
-                },
-                label = {
-                    truncateAtChars = 20,
-                    padding = 1,
-                    marginLeft = 0,
-                    sameKindSeparator = ", ",
-                },
-                extmark = {
-                    priority = 50,
-                },
-                autoEnableHints = true,
-            })
-        end,
-    },
 }
