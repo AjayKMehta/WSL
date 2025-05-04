8a7372db6384d07242e3c03535cfde10933bcfca
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-05-04 15:07:16 -0700

    Update DAP configuration for C#.

--- home/dot_config/nvim/lua/configs/dap.lua
+++ home/dot_config/nvim/lua/configs/dap.lua
@@ -238,22 +238,20 @@ dap.configurations.cs = {
         name = "launch - netcoredbg",
         type = "coreclr",
         request = "launch",
-        justMyCode = false,
-        stopAtEntry = false,
         env = function()
             local dll = ensure_dll()
-            local vars = dotnet.get_environment_variables(dll.project_name, dll.relative_project_path)
+            local vars = dotnet.get_environment_variables(dll.project_name, dll.absolute_project_path, false)
             return vars or nil
         end,
         program = function()
             local dll = ensure_dll()
             local co = coroutine.running()
             rebuild_project(co, dll.project_path)
-            return dll.relative_dll_path
+            return dll.target_path
         end,
         cwd = function()
             local dll = ensure_dll()
-            return dll.relative_project_path
+            return dll.absolute_project_path
         end,
     },
     {
