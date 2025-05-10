f1f7e5979601638f285177675c2322fb0ddccc62
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-05-10 10:01:13 -0700

    Update TOML LSP.

--- home/dot_config/nvim/lsp/taplo.lua
+++ home/dot_config/nvim/lsp/taplo.lua
@@ -1,26 +1,33 @@
 return {
-  cmd = { "taplo", "lsp", "stdio" },
-  filetypes = { "toml" },
-  root_markers = { ".git" },
-  settings = {
-    -- See all the setting options
-    -- https://github.com/tamasfe/taplo/blob/master/editors/vscode/package.json
-    evenBetterToml = {
-      taplo = {
-        configFile = {
-          enabled = true,
+    cmd = { "taplo", "lsp", "stdio" },
+    filetypes = { "toml" },
+    root_markers = { ".git", ".toml" },
+    settings = {
+        -- See all the setting options
+        -- https://github.com/tamasfe/taplo/blob/master/editors/vscode/package.json
+        evenBetterToml = {
+            taplo = {
+                bundled = true,
+                configFile = {
+                    enabled = true,
+                },
+            },
+            semanticTokens = true,
+            schema = {
+                enabled = true,
+                links = true,
+                catalogs = {
+                    "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/api/json/catalog.json",
+                    "https://taplo.tamasfe.dev/schema_index.json",
+                },
+                cache = {
+                    memoryExpiration = 60,
+                    diskExpiration = 600,
+                },
+            },
+            completion = {
+                maxKeys = 10, -- default: 5
+            },
         },
-      },
-      schema = {
-        enabled = true,
-        catalogs = {
-          "https://www.schemastore.org/api/json/catalog.json",
-        },
-        cache = {
-          memoryExpiration = 60,
-          diskExpiration = 600,
-        },
-      },
     },
-  },
 }
