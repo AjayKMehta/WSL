0da8055b0e2f3ad4821cf210af483c8d0f00f20d
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-05-28 21:29:09 -0700

    Update autocmd for roslyn to insert doc comments.

--- home/dot_config/nvim/lua/autocmds.lua
+++ home/dot_config/nvim/lua/autocmds.lua
@@ -147,31 +147,56 @@ autocmd({ "InsertLeave" }, {
 })
 
 -- https://github.com/seblyng/roslyn.nvim/wiki#textdocument_vs_onautoinsert
-autocmd('InsertCharPre', {
-    pattern = '*.cs',
-    callback = function()
-    local char = vim.v.char
+autocmd("LspAttach", {
+    callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
 
-    if char ~= "/" then
-        return
+    if client and (client.name == "roslyn" or client.name == "roslyn_ls") then
+        vim.api.nvim_create_autocmd("InsertCharPre", {
+            desc = "Roslyn: Trigger an auto insert on '/'.",
+            buffer = bufnr,
+            callback = function()
+                local char = vim.v.char
+
+                if char ~= "/" then
+                    return
+                end
+
+                local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+                row, col = row - 1, col + 1
+                local uri = vim.uri_from_bufnr(bufnr)
+
+                local params = {
+                    _vs_textDocument = { uri = uri },
+                    _vs_position = { line = row, character = col },
+                    _vs_ch = char,
+                    _vs_options = {
+                        tabSize = vim.bo[bufnr].tabstop,
+                        insertSpaces = vim.bo[bufnr].expandtab,
+                    },
+                }
+
+                -- NOTE: We should send textDocument/_vs_onAutoInsert request only after
+                -- buffer has changed.
+                vim.defer_fn(function()
+                    client:request(
+                        ---@diagnostic disable-next-line: param-type-mismatch
+                        "textDocument/_vs_onAutoInsert",
+                        params,
+                        function(err, result, _)
+                            if err or not result then
+                                return
+                            end
+
+                            vim.snippet.expand(result._vs_textEdit.newText)
+                        end,
+                        bufnr
+                    )
+                end, 1)
+            end,
+        })
     end
-
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    row, col = row - 1, col + 1
-    local bufnr = vim.api.nvim_get_current_buf()
-    local uri = vim.uri_from_bufnr(bufnr)
-
-    local params = {
-        _vs_textDocument = { uri = uri },
-        _vs_position = { line = row, character = col },
-        _vs_ch = char,
-        _vs_options = { tabSize = 4, insertSpaces = true },
-    }
-
-    -- NOTE: we should send textDocument/_vs_onAutoInsert request only after buffer has changed.
-    vim.defer_fn(function()
-        vim.lsp.buf_request(bufnr, "textDocument/_vs_onAutoInsert", params)
-    end, 1)
     end,
   })
 
