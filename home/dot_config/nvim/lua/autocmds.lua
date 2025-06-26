b8db19d24c0f0274c26f9f19e720d1281af7f4ab
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-06-26 14:20:50 -0700

    Fix for error running checkhealth for render-markdown.

--- home/dot_config/nvim/lua/autocmds.lua
+++ home/dot_config/nvim/lua/autocmds.lua
@@ -149,56 +149,56 @@ autocmd({ "InsertLeave" }, {
 -- https://github.com/seblyng/roslyn.nvim/wiki#textdocument_vs_onautoinsert
 autocmd("LspAttach", {
     callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local bufnr = args.buf
 
-    if client and (client.name == "roslyn" or client.name == "roslyn_ls") then
-        vim.api.nvim_create_autocmd("InsertCharPre", {
-            desc = "Roslyn: Trigger an auto insert on '/'.",
-            buffer = bufnr,
-            callback = function()
-                local char = vim.v.char
+        if client and (client.name == "roslyn" or client.name == "roslyn_ls") then
+            vim.api.nvim_create_autocmd("InsertCharPre", {
+                desc = "Roslyn: Trigger an auto insert on '/'.",
+                buffer = bufnr,
+                callback = function()
+                    local char = vim.v.char
 
-                if char ~= "/" then
-                    return
-                end
+                    if char ~= "/" then
+                        return
+                    end
 
-                local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-                row, col = row - 1, col + 1
-                local uri = vim.uri_from_bufnr(bufnr)
+                    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+                    row, col = row - 1, col + 1
+                    local uri = vim.uri_from_bufnr(bufnr)
 
-                local params = {
-                    _vs_textDocument = { uri = uri },
-                    _vs_position = { line = row, character = col },
-                    _vs_ch = char,
-                    _vs_options = {
-                        tabSize = vim.bo[bufnr].tabstop,
-                        insertSpaces = vim.bo[bufnr].expandtab,
-                    },
-                }
+                    local params = {
+                        _vs_textDocument = { uri = uri },
+                        _vs_position = { line = row, character = col },
+                        _vs_ch = char,
+                        _vs_options = {
+                            tabSize = vim.bo[bufnr].tabstop,
+                            insertSpaces = vim.bo[bufnr].expandtab,
+                        },
+                    }
 
-                -- NOTE: We should send textDocument/_vs_onAutoInsert request only after
-                -- buffer has changed.
-                vim.defer_fn(function()
-                    client:request(
+                    -- NOTE: We should send textDocument/_vs_onAutoInsert request only after
+                    -- buffer has changed.
+                    vim.defer_fn(function()
+                        client:request(
                         ---@diagnostic disable-next-line: param-type-mismatch
-                        "textDocument/_vs_onAutoInsert",
-                        params,
-                        function(err, result, _)
-                            if err or not result then
-                                return
-                            end
+                            "textDocument/_vs_onAutoInsert",
+                            params,
+                            function(err, result, _)
+                                if err or not result then
+                                    return
+                                end
 
-                            vim.snippet.expand(result._vs_textEdit.newText)
-                        end,
-                        bufnr
-                    )
-                end, 1)
-            end,
-        })
-    end
+                                vim.snippet.expand(result._vs_textEdit.newText)
+                            end,
+                            bufnr
+                        )
+                    end, 1)
+                end,
+            })
+        end
     end,
-  })
+})
 
 -- https://github.com/olimorris/codecompanion.nvim/discussions/640#discussioncomment-12057645
 -- https://github.com/olimorris/codecompanion.nvim/discussions/640#discussioncomment-12057645
@@ -381,5 +381,50 @@ autocmd("LspAttach", {
             local win = vim.api.nvim_get_current_win()
             vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
         end
+    end,
+})
+
+-- TODO: Look into solution based on installed parsers, i.e. no hardcoding.
+-- Based on https://www.reddit.com/r/neovim/comments/1kuj9xm/comment/mubg2cm
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "bash",
+        "c",
+        "cmake",
+        "codecompanion",
+        "css",
+        "diff",
+        "dockerfile",
+        "gitconfig",
+        "gitrebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "gpg",
+        "haskell",
+        "html",
+        "http",
+        "javascript",
+        "jq",
+        "json",
+        "jsonc",
+        "lua",
+        "make",
+        "markdown",
+        "mermaid",
+        "ps1", -- PowerShell
+        "python",
+        "quarto",
+        "r",
+        "sql",
+        "toml",
+        "typescript",
+        "vim",
+        "help", -- for vimdoc
+        "xml",
+        "yaml",
+    },
+    callback = function()
+        vim.treesitter.start()
     end,
 })
