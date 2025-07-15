d9e3ebc88fe04f308df7cf393cce62606e1e22ae
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-07-14 20:28:28 -0700

    Add plugins for breakpoints.

--- home/dot_config/nvim/lua/plugins/debug.lua
+++ home/dot_config/nvim/lua/plugins/debug.lua
@@ -1,29 +1,40 @@
 local load_config = require("utils").load_config
 
+local map_desc = require("utils.mappings").map_desc
+
 return {
     {
         "mfussenegger/nvim-dap",
         config = load_config("dap"),
         dependencies = {
-            { {
-                -- https://igorlfs.github.io/nvim-dap-view/faq#why-add-nvim-dap-view-as-a-dependency-for-nvim-dap
-                "igorlfs/nvim-dap-view",
-                cmd = { "DapViewOpen", "DapViewClose", "DapViewToggle", "DapViewWatch" },
-                opts = {
-                    winbar = {
-                        sections = { "console", "watches", "scopes", "exceptions", "breakpoints", "threads", "repl" },
-                    },
-                    windows = {
-                        terminal = {
-                            position = "left",
-                            -- List of debug adapters for which the terminal should be ALWAYS hidden
-                            hide = {},
-                            -- Hide the terminal when starting a new session
-                            start_hidden = true,
+            {
+                {
+                    -- https://igorlfs.github.io/nvim-dap-view/faq#why-add-nvim-dap-view-as-a-dependency-for-nvim-dap
+                    "igorlfs/nvim-dap-view",
+                    cmd = { "DapViewOpen", "DapViewClose", "DapViewToggle", "DapViewWatch" },
+                    opts = {
+                        winbar = {
+                            sections = {
+                                "console",
+                                "watches",
+                                "scopes",
+                                "exceptions",
+                                "breakpoints",
+                                "threads",
+                                "repl",
+                            },
+                        },
+                        windows = {
+                            terminal = {
+                                position = "left",
+                                -- List of debug adapters for which the terminal should be ALWAYS hidden
+                                hide = {},
+                                -- Hide the terminal when starting a new session
+                                start_hidden = true,
+                            },
                         },
                     },
                 },
-            },
                 {
                     "Joakker/lua-json5",
                     build = "./install.sh",
@@ -117,6 +128,32 @@ return {
                 },
             },
         },
+    },
+    {
+        "Carcuis/dap-breakpoints.nvim",
+        cmd = { "DapBpToggle", "DapBpNext", "DapBpPrev", "DapBpReveal" },
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("persistent-breakpoints").setup({
+                load_breakpoints_event = { "BufReadPost" },
+                always_reload = true,
+            })
+            require("dap-breakpoints").setup()
+
+            local dapbp_api = require("dap-breakpoints.api")
+            map_desc("n", "<leader>dth", dapbp_api.set_hit_condition_breakpoint, "Set Hit Condition Breakpoint")
+            map_desc("n", "<leader>dtL", function()
+                dapbp_api.load_breakpoints({ notify = true })
+            end, "Load Breakpoints")
+            map_desc("n", "<leader>dtS", function()
+                dapbp_api.save_breakpoints({ notify = true })
+            end, "Save Breakpoints")
+            map_desc("n", "<leader>dte", dapbp_api.edit_property, "Edit Breakpoint Property")
+            map_desc("n", "<leader>dtv", dapbp_api.toggle_virtual_text, "Toggle Breakpoint Virtual Text")
+            map_desc("n", "[,", dapbp_api.go_to_previous, "Go to Previous Breakpoint")
+            map_desc("n", "],", dapbp_api.go_to_next, "Go to Next Breakpoint")
+        end,
+        dependencies = { "Weissle/persistent-breakpoints.nvim", "mfussenegger/nvim-dap" },
     },
     {
         -- It's important that you set up the plugins in the following order:
