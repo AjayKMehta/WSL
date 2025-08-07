ef966cf03218fe8e13785539ed979236db782fab
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-08-06 20:19:41 -0700

    Add config for quicker and log-highlight.

--- home/dot_config/nvim/lua/plugins/utility.lua
+++ home/dot_config/nvim/lua/plugins/utility.lua
@@ -839,4 +839,32 @@ return {
             { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
         },
     },
+    {
+        "fei6409/log-highlight.nvim",
+        config = function()
+            require("log-highlight").setup({})
+        end,
+    },
+    {
+        "stevearc/quicker.nvim",
+        event = "FileType qf",
+        opts = {
+            keys = {
+                {
+                    ">",
+                    function()
+                        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+                    end,
+                    desc = "Expand quickfix context",
+                },
+                {
+                    "<",
+                    function()
+                        require("quicker").collapse()
+                    end,
+                    desc = "Collapse quickfix context",
+                },
+            },
+        },
+    },
 }
