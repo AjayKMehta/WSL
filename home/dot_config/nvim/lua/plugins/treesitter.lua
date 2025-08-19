297903fb81d2d739e0ad163f518c912e73b11980
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-08-19 16:26:24 -0700

    mise + treesitter

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -16,6 +16,14 @@ return {
         },
         build = ":TSUpdate",
         config = load_config("treesitter"),
+        -- https://mise.jdx.dev/mise-cookbook/neovim.html#code-highlight-for-run-commands
+        init = function()
+            require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+                local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+                local filename = vim.fn.fnamemodify(filepath, ":t")
+                return string.match(filename, ".*mise.*%.toml$") ~= nil
+            end, { force = true, all = false })
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
