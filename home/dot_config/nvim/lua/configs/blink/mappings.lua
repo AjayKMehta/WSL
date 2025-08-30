199e38cbd75d2c8d6d41edf134ca80f013550719
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-08-29 20:45:47 -0700

    Update blink.cmp keymaps.

--- home/dot_config/nvim/lua/configs/blink/mappings.lua
+++ home/dot_config/nvim/lua/configs/blink/mappings.lua
@@ -11,13 +11,69 @@
 --
 -- See :h blink-cmp-config-keymap for defining your own keymap
 
--- TODO: set up <M-d>, <C-j>, <C-k>, <Down>, <Up>, <C-y> like nvim-cmp?
 return {
     preset = "default",
     -- https://www.reddit.com/r/neovim/comments/1kdg687/how_do_you_reopen_completion_menu_in_blinkcmp/
     ["<C-s>"] = { "show", "show_signature", "hide_signature" },
-    ["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
+    ["<C-d>"] = { "show", "show_documentation", "hide_documentation" },
     ["<C-e>"] = { "hide", "show" },
+    ["<C-Space>"] = { "show" },
+    ["<CR>"] = { "accept", "fallback" },
+
+    ["<C-k>"] = {
+        function(cmp)
+            local ls = require("luasnip")
+            if ls.expand_or_jumpable() then
+                return ls.expand_or_jump()
+            end
+        end,
+        "fallback",
+    },
+    ["<C-j>"] = {
+        function(cmp)
+            local ls = require("luasnip")
+            if ls.jumpable(-1) then
+                return ls.jump(-1)
+            end
+        end,
+        "fallback",
+    },
+    ["<C-u>"] = {
+        function(cmp)
+            local ls = require("luasnip")
+            if ls.choice_active() then
+                return require("luasnip.extras.select_choice")()
+            end
+        end,
+        "fallback",
+    },
+    ["<C-n>"] = {
+        function(cmp)
+            local bc = require("blink.cmp")
+            local ls = require("luasnip")
+            if ls.choice_active() then
+                ls.change_choice(1)
+            elseif bc.is_visible() then
+                bc.select_next()
+            end
+        end,
+        "fallback",
+    },
+    ["<C-p>"] = {
+        function(cmp)
+            local bc = require("blink.cmp")
+            local ls = require("luasnip")
+            if ls.choice_active() then
+                ls.change_choice(-1)
+            elseif bc.is_visible() then
+                bc.select_prev()
+            end
+        end,
+        "fallback",
+    },
+
+    ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+    ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 
     -- Select Nth item from the list
     ["<A-1>"] = {
