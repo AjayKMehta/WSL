c3ab5d9e0cd57a078ba48a4c51a7d9a6a6b672bd
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-08-29 20:42:28 -0700

    Fix silly mistake.

--- home/dot_config/nvim/lua/configs/nvim_cmp/mappings.lua
+++ home/dot_config/nvim/lua/configs/nvim_cmp/mappings.lua
@@ -1,25 +1,23 @@
-local cmp = require("cmp")
-local ls = require("luasnip")
 return {
     -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#safely-select-entries-with-cr
-    ["<CR>"] = cmp.mapping({
+    ["<CR>"] = require("cmp").mapping({
         i = function(fallback)
-            local cmp = cmp
+            local cmp = require("cmp")
             if cmp.visible() and cmp.get_active_entry() then
                 cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
             else
                 fallback()
             end
         end,
-        s = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
+        s = require("cmp").mapping.confirm({ select = true }),
+        c = require("cmp").mapping.confirm({
+            behavior = require("cmp").ConfirmBehavior.Replace,
             select = false,
         }),
     }),
-    ["<M-d>"] = cmp.mapping({
+    ["<C-d>"] = require("cmp").mapping({
         i = function()
-            local cmp = cmp
+            local cmp = require("cmp")
             if cmp.visible_docs() then
                 cmp.close_docs()
             else
@@ -27,27 +25,30 @@ return {
             end
         end,
     }),
-    ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }), -- Alternative `Select Previous Item`
-    ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }), -- Alternative `Select Next Item`
-    ["<C-y>"] = cmp.mapping(
-        cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
-        { "i", "c" }
+    ["<Down>"] = require("cmp").mapping(require("cmp").mapping.select_next_item(), { "i", "c" }), -- Alternative `Select Previous Item`
+    ["<Up>"] = require("cmp").mapping(require("cmp").mapping.select_prev_item(), { "i", "c" }), -- Alternative `Select Next Item`
+    ["<C-y>"] = require("cmp").mapping(
+        require("cmp").mapping.confirm({ behavior = require("cmp").ConfirmBehavior.Insert, select = true }),
+        { "i" }
     ), -- Use to select current item
-    ["<C-k>"] = cmp.mapping(function(fallback)
+    ["<C-k>"] = require("cmp").mapping(function(fallback)
+        local ls = require("luasnip")
         if ls.expand_or_jumpable() then
             ls.expand_or_jump()
         else
             fallback()
         end
     end, { "i", "s" }),
-    ["<C-j>"] = cmp.mapping(function(fallback)
+    ["<C-j>"] = require("cmp").mapping(function(fallback)
+        local ls = require("luasnip")
         if ls.jumpable(-1) then
             ls.jump(-1)
         else
             fallback()
         end
     end, { "i", "s" }),
-    ["<C-u>"] = cmp.mapping(function(fallback)
+    ["<C-u>"] = require("cmp").mapping(function(fallback)
+        local ls = require("luasnip")
         if ls.choice_active() then
             require("luasnip.extras.select_choice")()
         else
@@ -56,20 +57,21 @@ return {
     end, { "i", "s" }),
     -- Tab and Shift + Tab help navigate between snippet nodes.
     -- Complete common string (similar to shell completion behavior).
-    ["<C-l>"] = cmp.mapping(function(fallback)
+    ["<C-l>"] = require("cmp").mapping(function(fallback)
+        local cmp = require("cmp")
         if cmp.visible() then
             return cmp.complete_common_string()
         end
         fallback()
     end, { "i", "c" }),
     -- https://www.dmsussman.org/resources/neovimsetup/
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping({
-        i = cmp.mapping.abort(),
+    ["<C-Space>"] = require("cmp").mapping(require("cmp").mapping.complete(), { "i", "c" }),
+    ["<C-b>"] = require("cmp").mapping.scroll_docs(-4),
+    ["<C-f>"] = require("cmp").mapping.scroll_docs(4),
+    ["<C-e>"] = require("cmp").mapping({
+        i = require("cmp").mapping.abort(),
         c = function()
-            local cmp = cmp
+            local cmp = require("cmp")
             if cmp.visible() then
                 cmp.mapping.close()
             end
@@ -77,9 +79,8 @@ return {
     }),
     ["<C-n>"] = {
         i = function()
-            local cmp = cmp
-            local ls = ls
-
+            local cmp = require("cmp")
+            local ls = require("luasnip")
             if ls.choice_active() then
                 ls.change_choice(1)
             elseif cmp.visible() then
@@ -92,9 +93,8 @@ return {
     },
     ["<C-p>"] = {
         i = function()
-            local cmp = cmp
-            local ls = ls
-
+            local cmp = require("cmp")
+            local ls = require("luasnip")
             if ls.choice_active() then
                 ls.change_choice(-1)
             elseif cmp.visible() then
@@ -104,7 +104,9 @@ return {
             end
         end,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<Tab>"] = require("cmp").mapping(function(fallback)
+        local cmp = require("cmp")
+        local ls = require("luasnip")
         if cmp.visible() then
             cmp.select_next_item()
         elseif ls.expand_or_jumpable() then
@@ -114,7 +116,9 @@ return {
         end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<S-Tab>"] = require("cmp").mapping(function(fallback)
+        local cmp = require("cmp")
+        local ls = require("luasnip")
         if cmp.visible() then
             cmp.select_prev_item()
         elseif ls.jumpable(-1) then
