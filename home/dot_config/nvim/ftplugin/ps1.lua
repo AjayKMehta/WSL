e7a44d72fe790f1a3506a8d0854188c7c54bacc7
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-09-10 21:28:11 -0700

    Fix error for bad command.

--- home/dot_config/nvim/ftplugin/ps1.lua
+++ home/dot_config/nvim/ftplugin/ps1.lua
@@ -1,12 +1,5 @@
 -- https://github.com/PowerShell/PowerShellEditorServices/blob/main/docs/guide/getting_started.md#indentation
 
--- disable indent from powershell treesitter parser
--- because the parse isn't mature currently
--- you can ignore this step if don't use treesitter
-if pcall(require, 'nvim-treesitter') then
-  vim.schedule(function() vim.cmd([[TSBufDisable indent]]) end)
-end
-
 vim.opt_local.cindent = true
 vim.opt_local.cinoptions:append { 'J1', '(1s', '+0' } -- see :h cino-J, cino-(, cino-+
 
