7154f5ef8167ff380fe5d62b6f360de1051d7ab8
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-01-13 22:29:55 -0800

    Remove deleted file.

--- home/dot_config/nvim/lua/configs/md_preview.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/iamcco/markdown-preview.nvim/issues/558#issuecomment-1739832086
-vim.g.mkdp_auto_close = true
-vim.g.mkdp_open_to_the_world = false
-vim.g.mkdp_open_ip = "127.0.0.1"
-vim.g.mkdp_port = "8888"
-vim.g.mkdp_browser = ""
-vim.g.mkdp_echo_preview_url = true
-vim.g.mkdp_page_title = "${name}"
