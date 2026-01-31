01b3f47fc14c1cd6f883cc5e4533be7395063d38
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-01-31 10:58:55 -0800

    Update config for nvim-highlight-colors.

--- home/dot_config/nvim/lua/plugins/utility.lua
+++ home/dot_config/nvim/lua/plugins/utility.lua
@@ -332,6 +332,27 @@ return {
             ---Set virtual symbol (requires render to be set to 'virtual')
             virtual_symbol = "■",
 
+            ---Highlight hex colors, e.g. '#FFFFFF'
+            enable_hex = true,
+
+            ---Highlight short hex colors e.g. '#fff'
+            enable_short_hex = true,
+
+            ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+            enable_rgb = true,
+
+            ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+            enable_hsl = true,
+
+            ---Highlight ansi colors, e.g '\033[0;34m'
+            enable_ansi = true,
+
+            ---Highlight xterm 256 (8bit) colors, e.g '\033[38;5;118m'
+            enable_xterm256 = true,
+
+            ---Highlight xterm True Color (24bit) colors, e.g '\033[38;2;118;64;90m'
+            enable_xtermTrueColor = true,
+
             ---Highlight named colors, e.g. 'green'
             enable_named_colors = true,
 
