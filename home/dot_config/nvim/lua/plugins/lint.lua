ed4b6de60b7865b83fbdea158ba8021e4fbb7b04
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-04-02 18:00:53 -0700

    Add panache and update nvim-lint config.

--- home/dot_config/nvim/lua/plugins/lint.lua
+++ home/dot_config/nvim/lua/plugins/lint.lua
@@ -3,72 +3,56 @@ return {
         -- An asynchronous linter plugin
         "mfussenegger/nvim-lint",
         event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            -- Event to trigger linters
-            events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-            linters_by_ft = {
+        config = function()
+            local lint = require("lint")
+
+            lint.linters_by_ft = {
                 bash = { "shellcheck" },
                 dockerfile = { "hadolint" },
                 editorconfig = { "editorconfig-checker" },
+                ghaction = { "actionlint", "zizmor" },
                 haskell = { "hlint" },
                 html = { "tidy" },
                 json = { "jsonlint" },
                 jsonc = { "jsonlint" },
                 lua = { "selene" },
                 markdown = { "markdownlint-cli2" },
-                python = { "mypy", "ruff" },
+                python = { "ruff" },
+                quarto = { "panache" },
+                rmd = { "panache" },
                 sh = { "shellcheck" },
-                tex = { "chktex", "lacheck" },
+                tex = { "chktex", },
                 yaml = { "yamllint", "yq" },
-                ghaction = { "actionlint", "zizmor" },
-            },
-        },
-        config = function(_, opts)
-            local lint = require("lint")
+            }
 
-            -- See https://github.com/petobens/dotfiles/blob/08ef71fe8772cd107ac0a141cdefabf9cf2acb93/nvim/lua/plugin-config/nvimlint_config.lua
             -- Linter config/args
             local linters = lint.linters
             ---- Markdown
             linters["markdownlint-cli2"].args = { "--config=" .. vim.env.HOME .. "/.markdownlint.json" }
-            ---- Python
-            local ruff_severities = {
-                ["E"] = vim.diagnostic.severity.ERROR,
-                ["F8"] = vim.diagnostic.severity.ERROR,
-                ["F"] = vim.diagnostic.severity.WARN,
-                ["C4"] = vim.diagnostic.severity.WARN,
-                ["EM"] = vim.diagnostic.severity.WARN,
-                ["W"] = vim.diagnostic.severity.WARN,
-                ["D"] = vim.diagnostic.severity.INFO,
-                ["B"] = vim.diagnostic.severity.INFO,
+
+            -- Quarto
+            local function get_file_name()
+                return vim.api.nvim_buf_get_name(0)
+            end
+
+            local severity_map = {
+                ["error"] = vim.diagnostic.severity.ERROR,
+                ["warning"] = vim.diagnostic.severity.WARN,
+                ["information"] = vim.diagnostic.severity.INFO,
             }
-            local ruff_parser = linters.ruff.parser
-            local function starts_with(str, start)
-                return str:sub(1, #start) == start
-            end
 
-            linters.ruff.parser = function(output, bufnr)
-                local diagnostics = ruff_parser(output, bufnr)
-                for _, v in pairs(diagnostics) do
-                    if starts_with(v.code, "RET") then
-                        return vim.diagnostic.severity.WARN
-                    end
+            -- warning[undefined-reference-label]: Reference label '[2]' not found at <stdin>:1:5
+            local pattern = "(%w+)%[([^%]]+)%]:%s?(.+)%s+([^:]+):(%d+):(%d+)"
+            -- This also works: (%w+)%[(%S+)%]:%s?([^:]+)%s(%S+):(%d+):(%d+)
+            local groups = { "severity", "code", "message", "file", "lnum", "col" }
+            local defaults = {["source"] = "panache"}
 
-                    local code = string.sub(v.code, 1, 2)
+            linters.panache = {
+                cmd = "panache",
+                args = { "lint", "--message-format", "short", "--stdin-filename", get_file_name },
+                parser = require('lint.parser').from_pattern(pattern, groups, severity_map, defaults)
+            }
 
-                    if code ~= "F8" or code ~= "C4" or code ~= "EM" then
-                        code = string.sub(code, 1, 1)
-                    end
-                    local new_severity = ruff_severities[code]
-                    if new_severity ~= nil then
-                        v.severity = new_severity
-                    else
-                        -- Default to INFO if not mapped.
-                        v.severity = vim.diagnostic.severity.WARN
-                    end
-                end
-                return diagnostics
-            end
             ---- TeX
             linters.chktex.args = vim.list_extend(vim.deepcopy(linters.chktex.args), {
                 "-wall",
